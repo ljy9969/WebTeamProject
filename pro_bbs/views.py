6db9dec967fa06cf25f4fba8829beceadda52027
kch8906 <kch8906@naver.com>
2022-02-27 04:28:01 +0900

    add username and comment count in a probbs

--- pro_bbs/views.py
+++ pro_bbs/views.py
@@ -3,13 +3,14 @@ from django.utils import timezone
 from pro_bbs.models import Question, Answer
 from pro_bbs.forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
+
 
 
 # Create your views here.
 
 
 def index(request):
-
     page = request.GET.get('page', '1')
     question_list = Question.objects.all().order_by('-create_date')
 
@@ -26,12 +27,14 @@ def detail(request, question_id):
     return render(request, 'pro_bbs/bbs_detail.html', context)
 
 
+@login_required(login_url='users:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     if request.method == "POST":
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
@@ -42,11 +45,13 @@ def answer_create(request, question_id):
     return render(request, 'pro_bbs/bbs_questionForm.html', context)
 
 
+@login_required(login_url='users:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)  # 임시 저장을 의미
+            question.author = request.user
             question.create_date = timezone.now()
             question.save()
             return redirect('probbs:index')
