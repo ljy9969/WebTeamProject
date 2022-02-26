2276b34f964964b95805c780aa1f2518e24a4325
kch8906 <kch8906@naver.com>
2022-02-26 20:28:24 +0900

    project files upload

---
+++ corona_19/urls.py
@@ -0,0 +1,14 @@
+# Root URLConf(http://127.0.0.1:8000/)
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),  # Home Page
+    path('admin/', admin.site.urls),                                          # Admin Page
+    path('main/', include('main.urls')),                                      # login 후 Home Page
+    path('users/', include('users.urls')),                                    # login, 회원가입 page
+] + static(settings.MEDIA_URL,
+           document_root=settings.MEDIA_ROOT)
