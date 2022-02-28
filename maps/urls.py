1ab549ed18b38da003f0f9dc946464e65884e839
kch8906 <kch8906@naver.com>
2022-02-28 21:16:12 +0900

    Reset my branch

--- maps/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from maps import views
-
-app_name = 'map'
-urlpatterns = [
-    path('positions/', views.pos_hospital, name='pos_hospital')
-]
--- static/pro_bbs/js/pro_bbs.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function (){
-    $('.delete').on('click', function (){
-        if(confirm("정말로 삭제 하시겠습니까?")){
-            document.location.href = $(this).data('uri')
-        }
-    })
-})
